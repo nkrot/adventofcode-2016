af172e0967d61fce41d4d0f3218d8c61211321c6
Nikolai Krot <talpus@gmail.com>
2021-05-11 01:23:44 +0200

    refactor: make simple code more sophisticated

--- day_05/solution.py
+++ day_05/solution.py
@@ -7,7 +7,7 @@
 import re
 import os
 import sys
-from typing import List
+from typing import Callable
 import hashlib
 
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
@@ -17,38 +17,76 @@ from aoc import utils
 DEBUG = False
 
 
-def solve_p1(line: str) -> int:
+class Password(object):
+
+    def __init__(self, maxlen):
+        self.maxlen = maxlen
+        self.chars = [None] * self.maxlen
+
+    def __len__(self):
+        return len(self.chars) - self.chars.count(None)
+
+    def __str__(self):
+        return "".join(str(ch) for ch in self.chars)
+
+    def __add__(self, char: str):
+        pos = self.chars.index(None)
+        self.chars[pos] = char
+        return self
+
+    def __getitem__(self, pos: int):
+        return self.chars[pos]
+
+    def __setitem__(self, pos: int, val: str):
+        self.chars[int(pos)] = val
+
+
+def digests(prefix):
+    '''Generate suitable MD5 digests'''
+    idx = -1
+    while True:
+        idx += 1
+        m = hashlib.md5()
+        m.update(bytes(prefix + str(idx), 'utf8'))
+        hexdig = m.hexdigest()
+        if hexdig.startswith('00000'):
+            yield(hexdig)
+
+
+def solve(line: str, compose: Callable) -> str:
+    '''Generic solver for both parts'''
+    passwd = Password(8)
+    for hexdig in digests(line):
+        passwd = compose(passwd, hexdig)
+        if len(passwd) >= 8:
+            break
+    return str(passwd)
+
+
+def solve_p1(line: str) -> str:
     """Solution to the 1st part of the challenge"""
-    password = ''
-    idx = -1
-    while len(password) < 8:
-        idx += 1
-        m = hashlib.md5()
-        m.update(bytes(line + str(idx), 'utf8'))
-        hexdig = m.hexdigest()
-        if hexdig.startswith('00000'):
-            password += hexdig[5]
-            if DEBUG:
-                print(f"Password: {password} from {hexdig}")
-    return password
+
+    def composer(password, digest):
+        res = password + digest[5]
+        if DEBUG:
+            print(f"Password: {password.chars} from {digest}")
+        return res
+
+    return solve(line, composer)
 
 
-def solve_p2(line: str) -> int:
+def solve_p2(line: str) -> str:
     """Solution to the 2nd part of the challenge"""
-    password = [None] * 8
-    idx = -1
-    while password.count(None):
-        idx += 1
-        m = hashlib.md5()
-        m.update(bytes(line + str(idx), 'utf8'))
-        hexdig = m.hexdigest()
-        if hexdig.startswith('00000'):
-            pos, char = hexdig[5:7]
-            if pos in '01234567' and password[int(pos)] is None:
-                password[int(pos)] = char
-                if DEBUG:
-                    print(f"Password: {password} from {hexdig}")
-    return "".join(password)
+
+    def composer(password, digest):
+        pos, char = digest[5:7]
+        if pos in '01234567' and password[int(pos)] is None:
+            password[int(pos)] = char
+            if DEBUG:
+                print(f"Password: {password.chars} from {digest}")
+        return password
+
+    return solve(line, composer)
 
 
 tests = [
